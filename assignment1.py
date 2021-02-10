ef609140f8c7993e217fe3f71d8c0bbe8d8be51f
Chris Brooks <chris.cbrooks@gmail.com>
2021-02-10 13:29:30 -0800

    Assignment 1

---
+++ assignment1.py
@@ -0,0 +1,63 @@
+### ENGR 102 Assignment 1
+### Your name here
+
+## step 1: converting units. Please complete each of these functions.
+## If you need the formulas, you can find them here: https://www.mathsisfun.com/metric-imperial-conversion-charts.html
+
+## convert miles to KM and return the result. Remove 'pass' and add your code.
+
+def milesToKM(miles) :
+    pass
+
+## convert kilometers to miles and return the result
+def KMToMiles(km) :
+    pass
+
+## convert kilograms to pounds and return the result.
+
+def KGToPounds(kg) :
+    pass
+
+## convert pounds to kilograms and return the result
+
+def poundsToKG(pounds) :
+    pass
+
+## convert liters to quarts and return the result
+def litersToQuarts(liters) :
+    pass
+
+## convert liters to quarts and return the result
+def quartsToLiters(quarts) :
+    pass
+
+
+### Question 2. write a function that takes a distance in miles and a velocity in MPH
+### and returns the equivalent velocity in KM/second. Make use of the functions from question 1 as necessary.
+
+def getKMPerSecond(distanceInMiles, mph) :
+    pass
+
+
+### Question 3. Write a function called tooHeavy. It should take as input a weight in pounds, convert
+### that to kilograms, and then return true if the object is greater than 10 kilograms, and false otherwise.
+### Make use of the functions from question 1 as necessary.
+
+def tooHeavy(weightInPounds) :
+    pass
+
+### Question 4.  Write a function that takes a list of five distances: [10 miles, 50 miles, 100 miles,
+## 250 miles, 1000 miles] converts each amount to km, and then prints out how
+## long it would take to travel that distance if we were traveling 110 km/hr.
+### Make use of the functions from question 1 as necessary.
+
+distanceList = [10,50,100,250,1000]
+
+def convertListOfDistances(distanceList) :
+    pass
+
+### question 5: Extend convertListOfDistances to print out "pack a lunch!" for all the trips greater than 2 hours.
+
+### question 6: you're done! Review the GitHub video to make sure you know how to submit your assignment,
+### and then use commit and push to add it to your GitHub repo.
+
---
+++ readme.md
@@ -0,0 +1,39 @@
+
+### Assignment 1. 
+
+### Due - Friday, Feb 19, 11:59 pm
+
+This assignment is intended to give you some practice writing basic Python code, including functions, parameters, loops and conditionals. You'll also learn how to set up your own gitHub repo; we'll be using that to submit this assignment.
+
+1. To begin, watch [this video](http://www.google.com), which will show you a little bit about GitHub and how to use it. 
+
+2. Then, go to GitHub Classroom to check out the assignment. Here's the URL: 
+
+3. For this assignment, we're going to write some functions to help us convert from English measurements to metric and back. I've provided a few empty functions to get you started.  
+   Please complete each of them.
+
+- milesToKM
+
+- KMToMiles
+
+- KGToPounds
+
+- poundsToKG 
+
+- litersToQuarts
+
+- quartsToLiters
+
+4.  write a function that takes a distance in miles and a velocity in MPH
+and returns the equivalent velocity in KM/second.
+
+5. Write a function called tooHeavy. It should take as input a weight in pounds,
+convert that to kilograms, and then return true if the object is greater 
+  than 10 kilograms, and false otherwise.
+  
+6 Write a function that takes a list of five distances: [10 miles, 50 miles, 100 miles, 250 miles, 1000 miles]
+, converts each amount to km, and then prints out how long it would take to travel that distance if we were traveling 110 km/hr.
+  
+7. Extend that function to print out "pack a lunch!" for all the trips greater than 2 hours.
+
+8. And you're done! Use git to check your code back into your repo.
